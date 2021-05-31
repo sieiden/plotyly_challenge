1e122992b1ddf15689bbee000aad20ded1fbf095
sieiden <sarahieiden@gmail.com>
2021-05-31 13:47:43 -0500

    bar graph now reversed

--- static/js/app.js
+++ static/js/app.js
@@ -20,7 +20,7 @@ function buildPlot(bio){
 
         //build horizontal bar chart
         let trace1 = {
-            y: toString(otu_ids).slice(0,10),
+            y: otu_ids.toString().slice(0,10),
             x: sample_values.slice(0,10),
             marker: otu_ids.slice(0,10),
             text: otu_labels,
@@ -29,11 +29,14 @@ function buildPlot(bio){
         };
         let data1 = [trace1];
         let layout = {
-            title: "Top 10 Bacteria Cultures Found"
+            title: "Top 10 Bacteria Cultures Found",
+            yaxis: {autorange: 'reversed'}
         }
         Plotly.newPlot('bar', data1, layout);
 
         //build bubble chart
+        let desired_maximum_marker_size = 200;
+        let size = [400, 600, 800, 1000];
         let trace2 = {
             x: otu_ids,
             y: sample_values,
@@ -41,18 +44,33 @@ function buildPlot(bio){
             mode: 'markers',
             marker:{
                 color: otu_ids,
-                size: sample_values
+                size: sample_values,
+                sizeref :2.0 * Math.max(...size) / (desired_maximum_marker_size**2),
+                sizemode: 'area'
             }
         };
         let data2 = [trace2];
         let layout2 = {
-            title: "Bacteria Cultures Per Sample"
+            title: "Bacteria Cultures Per Sample",
+            xaxis: {
+                title: "OTU ID",
+                titlefont :{
+                    size:16
+                }
+            },
+            yaxis: {
+                title: "NUMBER OF BACTERIA CULTURES",
+                titlefont :{
+                    size:12
+                }
+            }
         };
         Plotly.newPlot('bubble', data2, layout2);
 
         //build demographic info
         let filteredData2 = data.metadata.filter(d=>d.id === parseInt(bio));
         panel = d3.select("#sample-metadata");
+        panel.html('');
         filteredData2.forEach((i)=>{
             Object.entries(i).forEach(([key,value])=>{
                 let line = panel.append("p");
